d3dc530ff442a570850e3b031dcc8003d4f4674c
Rohini.R <rohinirachelr@gmail.com>
2024-07-22 17:31:22 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function Incorrect1(){
+    document.querySelector("#optionlist1").style.backgroundColor='red';
+    document.querySelector("#Answer1").style.backgroundColor='red';
+}
+
+function Incorrect2(){
+    document.querySelector("#optionlist2").style.backgroundColor='red';
+    document.querySelector("#Answer2").style.backgroundColor='red';
+}
+
+function Incorrect3(){
+    document.querySelector("#optionlist3").style.backgroundColor='red';
+    document.querySelector("#Answer3").style.backgroundColor='red';
+}
+
+function Correct(){
+    document.querySelector(".opt1").style.backgroundColor='green';
+    document.querySelector(".opt1").style.color='white';
+    document.querySelector(".ans1").style.backgroundColor='green';
+    document.querySelector(".ans1").style.color='white';
+}
